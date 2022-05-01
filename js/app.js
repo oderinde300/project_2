fad2ab34a095b62d599890c1d085b1db441076b1
oderinde300 <104003435+oderinde300@users.noreply.github.com>
2022-05-01 16:21:28 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ const span = document.querySelector('span');
 ratingNums.forEach((ratingNum) => {
     ratingNum.addEventListener('click', () => {
         ratingNum.style.backgroundColor = '#7C8898';
-        ratingNum.style.color = '#FFF';
-        span.textContent = ratingNum.textContent;
+//         ratingNum.style.color = '#FFF';
+//         span.textContent = ratingNum.textContent;
     });
     const mainPage = document.querySelector('.main');
     const thankYouPage = document.querySelector('.thank-you');
