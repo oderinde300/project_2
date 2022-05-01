b8aba12d97a03a6cb0d13eec6a47c8a86c341f45
oderinde300 <104003435+oderinde300@users.noreply.github.com>
2022-05-01 16:43:32 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ const span = document.querySelector('span');
 
 ratingNums.forEach((ratingNum) => {
     ratingNum.addEventListener('click', () => {
-        ratingNum.style.backgroundColor = '#7C8898';
-        ratingNum.style.color = '#FFF';
         span.textContent = ratingNum.textContent;
     });
     const mainPage = document.querySelector('.main');
